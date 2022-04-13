52109bb638d84baf57457cba2c93dba51098c68b
atombomben <71074932+atombomben@users.noreply.github.com>
2022-04-13 23:04:01 +0200

    Added player + animation
    Fixed collission

--- GameDevProject/Assets/MoveCube.cs
+++ GameDevProject/Assets/MoveCube.cs
@@ -2,6 +2,13 @@ using UnityEngine;
 
 public class MoveCube : MonoBehaviour
 {
+    Rigidbody2D rb2d;
+    Animator animator;
+
+void Start() {
+    rb2d = GetComponent<Rigidbody2D>();
+    animator = GetComponent<Animator>();
+}
 
 public float speed = .01f;
     void Update()
@@ -12,6 +19,17 @@ public float speed = .01f;
        Vector3 moveDirection = new Vector3(xDirection, zDirection, 0.0f);
        
        transform.position += moveDirection * speed;
+
+        if (xDirection != 0 || zDirection != 0) {
+            animator.SetFloat("X", xDirection);
+            animator.SetFloat("Y", zDirection);
+
+            animator.SetBool("IsWalking", true);
+        } else {
+            animator.SetBool("IsWalking", false);
+        }
+
+
     }
 
 
