ebe6f39989bddb7d2552c863a94641f29e918f26
Hans-Henrik <hhmadsen10@gmail.com>
2022-04-13 11:52:22 +0200

    Animation

--- GameDevProject/Assets/MoveCube.cs
+++ GameDevProject/Assets/MoveCube.cs
@@ -3,16 +3,38 @@ using UnityEngine;
 public class MoveCube : MonoBehaviour
 {
 
-public float speed = .01f;
+    
+public Animator animator;
+
+public float moveSpeed = 0.01f;
+
+public Rigidbody2D rb;
+
+
+Vector2 movement;
+
     void Update()
     {
        float xDirection = Input.GetAxis("Horizontal");
        float zDirection = Input.GetAxis("Vertical");
 
+       animator.SetFloat("Horizontal", xDirection);
+       animator.SetFloat("Vertical",zDirection);
+      
+
+       
+       
+       //spriteRenderer.flipX = movement.x < 0.01 ? true : false;
+       
+
        Vector3 moveDirection = new Vector3(xDirection, zDirection, 0.0f);
        
-       transform.position += moveDirection * speed;
+       transform.position += moveDirection * moveSpeed;
+
+      
     }
+
+   
 
 
   
