f550be3a5438ede43150f1c4a7c838ecd9641460
Hans-Henrik <hhmadsen10@gmail.com>
2022-05-03 20:57:48 +0200

    Added attack animaiton

--- GameDevProject/Assets/MoveCube.cs
+++ GameDevProject/Assets/MoveCube.cs
@@ -17,6 +17,27 @@ void Start(){
 
 Vector2 movement;
 
+
+public bool facingRight = true; //Depends on if your animation is by default facing right or left
+ 
+void FixedUpdate()
+    {
+        float h = Input.GetAxis("Horizontal");
+        if(h > 0 && !facingRight)
+            Flip();
+        else if(h < 0 && facingRight)
+            Flip();
+     }
+void Flip ()
+    {
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+
+
+
     void Update()
     {
        float xDirection = Input.GetAxis("Horizontal");
---
+++ GameDevProject/Assets/PlayerCombat.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCombat : MonoBehaviour
+{
+ 
+ public Animator animator;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Attack();
+        }
+    }
+
+    void Attack()
+    {
+        //Play an attack animation
+        animator.SetTrigger("Attack");
+    }
+
+}
+
+
+
+
