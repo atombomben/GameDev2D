f11e7471fc2ffb339b82640a018679a9ab7b98f5
Hans-Henrik <hhmadsen10@gmail.com>
2022-06-14 14:10:11 +0200

    sword, health, enemy health, animation done

--- GameDevProject/Assets/PlayerCombat.cs
+++ GameDevProject/Assets/PlayerCombat.cs
@@ -6,22 +6,58 @@ public class PlayerCombat : MonoBehaviour
 {
  
  public Animator animator;
+ public Transform attackPoint;
+ public float attackRange = 0.5f;
+ public LayerMask enemyLayers;
+
+ public int attackDamage = 40;
+
+ public float attackRate = 2f;
+ float nextAttackTime = 0f;
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+        if(Time.time >=nextAttackTime)
         {
-            Attack();
+             if(Input.GetKeyDown(KeyCode.Space))
+           {
+              Attack();
+              nextAttackTime = Time.time + 1f / attackRate;
+           }
         }
+       
     }
 
     void Attack()
     {
         //Play an attack animation
         animator.SetTrigger("Attack");
+        //Detect enemies in range of attack
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+        //Damage them
+        foreach(Collider2D enemy in hitEnemies)
+        {
+            enemy.GetComponent<Enemy>().TakeDamage(attackDamage);
+        }
+
     }
 
+
+    void OnDrawGizmosSelected()
+    {
+        if(attackPoint == null)
+           return;
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+    }
+
+    void testt()
+    {
+        if(Input.GetKeyDown(KeyCode.Q));
+    {
+        Debug.Log("Virker det her?");
+    }
+    }
 }
 
 
