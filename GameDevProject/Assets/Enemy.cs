b6717451face49a9eb641343a1c18dad2c24a7b1
Hans-Henrik <hhmadsen10@gmail.com>
2022-06-21 01:00:36 +0200

    enemy melee first commit
    it can attack animaiton but it bugs

--- GameDevProject/Assets/Enemy.cs
+++ GameDevProject/Assets/Enemy.cs
@@ -4,7 +4,22 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+
     public Animator animaitor;
+[SerializeField] private float attackCooldown;
+//[SerializeField] private int damage;
+//[SerializeField] private float range;
+//[SerializeField] private float colliderDistance;
+//[SerializeField] private BoxCollider2D boxCollder;
+
+public Transform attackPoint;
+public float attackRange = 0.5f;
+public float attackRate  = 2f;
+float nextAttackTime = 0f;
+[SerializeField] private LayerMask playerLayer;
+private float cooldownTimer = Mathf.Infinity;
+public int attackDamage = 40;
+
 
     public int maxHealth = 100;
     int curentHealth;
@@ -13,6 +28,73 @@ public class Enemy : MonoBehaviour
     {
         curentHealth = maxHealth;
     }
+
+    private void Awake()
+    {
+        animaitor = GetComponent<Animator>();
+    }
+
+
+    private void Update()
+    {
+        //Attack only when player is in range
+        cooldownTimer += Time.deltaTime;
+
+        if (PlayerInSight())
+        {
+             if (cooldownTimer >= attackCooldown)
+        {
+            //Attack
+            cooldownTimer = 0;
+            animaitor.SetTrigger("EnemyAttack");
+        }
+        }
+    }
+
+        bool PlayerInSight()
+       {
+         Collider2D[] hitPlayer = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, playerLayer);
+          foreach(Collider2D player in hitPlayer)
+        {
+           Debug.Log("Hit" + player.name);
+        }
+        return true;
+         
+      
+
+       }
+
+
+
+       void OnDrawGizmosSelected()
+       {
+        if (attackPoint == null)
+        return;
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+       }
+    /*private bool PlayerInSight()
+        {
+            RaycastHit2D hit = Physics2D.BoxCast(boxCollder.bounds.center + transform.right * range * transform.localScale.x* colliderDistance,
+             new Vector3(boxCollder.bounds.size.x * range, boxCollder.bounds.size.y, boxCollder.bounds.size.z),
+              0, Vector2.left, 0, playerLayer);
+            return hit.collider != null;
+        }*/
+
+
+     /*private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireCube(boxCollder.bounds.center + transform.right * range * transform.localScale.x * colliderDistance,
+             new Vector3(boxCollder.bounds.size.x * range, boxCollder.bounds.size.y, boxCollder.bounds.size.z));
+        }*/
+
+
+
+
+
+
+
+    
 
 
     public void TakeDamage(int damage)
